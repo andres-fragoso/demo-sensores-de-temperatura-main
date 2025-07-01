a86c7d3415206e94248be6472341e35855395c6f
andres-fragoso <149045037+andres-fragoso@users.noreply.github.com>
2025-06-30 17:59:15 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import {onMounted } from "vue";
+import SensorHub from "src/utils/SensorHub";
+
+defineOptions({
+  name: "App",
+});
+
+onMounted(async () => {
+  SensorHub.start();
+});
+</script>
